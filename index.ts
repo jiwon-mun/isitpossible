b68bb43b452cf153aa161b9d464b216d32eb65e4
jiwon-mun <jw.mun@birdview.kr>
2023-02-06 14:43:07 +0900

    save

--- index.ts
+++ index.ts
@@ -5,15 +5,23 @@ const { stdout } = execa(`git`, [`remote`, `-v`], {
 });
 
 stdout &&
-  stdout.addListener("data", (data) => {
+  stdout.addListener("data", async (data) => {
     const remotes: Array<string> = data.toString().split("\n");
 
     const remote = remotes
       .find((value) => {
-        return value.includes("birdviewdev/fe-monorepo.git");
+        return value.includes("jiwon-mun/isitpossible.git");
       })
       ?.split("\t")!;
-    console.log("remote", remote[remote.length - 1]);
+
+    const url = remote[remote.length - 1];
+    const tagName = Math.random().toString().slice(0, 5);
+    await execa("git", ["tag", tagName]);
+    console.log("url", url);
+    // await execa("git", [`push`, url, tagName], {
+    //   stdout: process.stdout,
+    // });
+    console.log("remote");
   });
 
 export default "test";
